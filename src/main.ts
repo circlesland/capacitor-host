08579cb4be7a8f81281cd0a8c827fed46cb82a44
Razvan Statescu <razvan@statescu.net>
2022-07-07 21:51:03 +0300

    implement routing and update package

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import '@circlesland/frame-app/style/tailwind.scss';
+
 import { App as CapacitorApp, URLOpenListenerEvent } from '@capacitor/app';
-import App from 'frame-app/src/App.svelte';
+import App from '@circlesland/frame-app/src/App.svelte';
 
 import { AUTH_API_CALLBACK, AUTH_API_URL } from '../config/api';
 
-import type { ICirclesCustomWindow } from "frame-app/types/types";
+import type { ICirclesCustomWindow } from "@circlesland/frame-app/types/types";
 declare global {
   interface Window extends ICirclesCustomWindow {}
 }
